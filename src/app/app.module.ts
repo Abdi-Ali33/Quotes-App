dc7a1a79bbc55fd41f93a6a235d816b2d61eba7f
Abdi Ali <alikune33@gmail.com>
2022-04-10 17:23:58 +0300

    creating quote deatils folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule
